46b64361d31b0cd428827e123fe5e4826c9238ce
trung.tran <trantrungse1209@gmail.com>
2024-01-12 15:29:48 +0700

    refactor footer

--- src/components/modules/footer/index.tsx
+++ src/components/modules/footer/index.tsx
@@ -7,34 +7,35 @@ import { MIcon } from "../../../assets/icon/m.icon";
 const Footer = () => {
   return (
     <div className="relative bg-[url('/src/assets/image/cover.jpg')] bg-no-repeat bg-center bg-cover w-full h-screen">
-      <div className="absolute inset-0 bg-[url('/src/assets/image/blue_inside.png')] bg-repeat bg-center"></div>
-      <div className="absolute top-0 left-0 right-0 bottom-0 container z-1 max-w-[1128px] mx-auto min-h-dvh flex items-center justify-center px-4">
-        <div className="flex flex-col lg:flex-row gap-5 bg-component-background p-12 rounded-xl lg:rounded-2xl w-full ">
-          <div className="flex flex-col justify-around w-full lg:w-1/2 text-center lg:text-left">
-            <div>
-              <p className="text-largest pb-6">Are You Ready to Join</p>
-              <img
-                alt="bitmesh logo"
-                className="mx-auto lg:mx-0"
-                src={BitmeshLogo}
-              />
+      <div className="h-dvh">
+        <div className="absolute inset-0 bg-[url('/src/assets/image/blue_inside.png')] bg-repeat bg-center"></div>
+        <div className="absolute top-0 left-0 right-0 bottom-0 container z-1 max-w-[1128px] mx-auto min-h-dvh flex items-center justify-center px-4">
+          <div className="flex flex-col lg:flex-row gap-5 bg-component-background p-12 rounded-xl lg:rounded-2xl w-full ">
+            <div className="flex flex-col justify-around w-full lg:w-1/2 text-center lg:text-left">
+              <div>
+                <p className="text-largest pb-6">Are You Ready to Join</p>
+                <img
+                  alt="bitmesh logo"
+                  className="mx-auto lg:mx-0"
+                  src={BitmeshLogo}
+                />
+              </div>
+              <div>
+                <button className="text-larger py-3 px-12 bg-gray-gradient border rounded border-white/10 mt-6 lg:mt-0">
+                  Lets Talk
+                </button>
+              </div>
             </div>
-            <div>
-              <button className="text-larger py-3 px-12 bg-gray-gradient border rounded border-white/10 mt-6 lg:mt-0">
-                Lets Talk
-              </button>
+            <div className="w-full lg:w-1/2 flex items-center justify-end order-first lg:order-none">
+              <img alt="" className="mx-auto lg:mx-none" src={CenterLogo} />
             </div>
-          </div>
-          <div className="w-full lg:w-1/2 flex items-center justify-end order-first lg:order-none">
-            <img alt="" className="mx-auto lg:mx-none" src={CenterLogo} />
           </div>
         </div>
       </div>
-      <div className="pt-[100vh]"></div>
-      <div className="bg-[#141414] py-12 mt-[100vh] lg:mt-0">
+      <div className="bg-[#141414] py-12 ">
         <div className="max-w-[1128px] mx-auto flex flex-col lg:flex-row justify-between items-center">
           <div className="flex flex-col justify-between">
-            <div className="">
+            <div>
               <p className="text-largest pb-4">Are You Ready to Join</p>
               <img
                 alt="bitmesh logo"
